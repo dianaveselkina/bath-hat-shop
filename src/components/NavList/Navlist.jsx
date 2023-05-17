518528b29b7e5772d063543ade89b4ad7ea87eee
dianaveselkin <vess_di@mail.ru>
2023-05-17 20:37:00 +0300

    add animation

--- src/components/NavList/Navlist.jsx
+++ src/components/NavList/Navlist.jsx
@@ -3,9 +3,12 @@ export const NavList = () => {
   return (
     <div className="navlist">
       <div className="navlist__image">
-        <p className="navlist__animation">
-          Самые лучшие шапки для бани из натуральной шерсти от Дианы Веселкиной
-        </p>
+        <div>
+          <p className="navlist__animation">
+            Самые лучшие шапки <br /> для бани <br /> из натуральной шерсти{' '}
+            <br /> от Дианы Веселкиной
+          </p>
+        </div>
       </div>
       <div className="navlist__nav">
         <ul className="nav__list">
